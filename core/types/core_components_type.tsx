1a1fa209e9f41ed87d3363a9315ea0bf0a7fd8fb
Pathinya <gift_01210@hotmail.com>
2022-04-06 15:01:20 +0700

    🎁 update: secondary_mini_button component

--- core/types/core_components_type.tsx
+++ core/types/core_components_type.tsx
@@ -8,3 +8,8 @@ export type secondaryButtonType = {
   title: string
   onClick: void
 }
+
+export type secondaryMiniButtonType = {
+  icon: string
+  onClick: void
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,26 +4,18 @@ import "tailwindcss/tailwind.css";
 import React from "react";
 import App from "next/app";
 import Head from "next/head";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head
-        children={
-          <>
-            <title>Cookify</title>
-            <meta
-              name="viewport"
-              content="initial-scale=1.0, width=device-width"
-            />
-            <Script
-              src="https://kit.fontawesome.com/9c44f5505c.js"
-              crossOrigin="anonymous"
-            ></Script>
-          </>
-        }
-      ></Head>
+      <Head>
+        <title>Cookify</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <script
+          src="https://kit.fontawesome.com/9c44f5505c.js"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <Component {...pageProps} />
     </>
   );
