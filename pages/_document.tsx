6fbbecbe5bcab4a7990e96e7a7350cccbb3f2c50
Pathinya <gift_01210@hotmail.com>
2022-04-22 22:16:51 +0700

    🎁 fix: font not loaded when deployed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <script src="https://kit.fontawesome.com/9c44f5505c.js" crossOrigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500&display=swap" rel="stylesheet"></link>
+        <link href="//fonts.googleapis.com/css2?family=Kanit:wght@400;500&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
         <Main />
