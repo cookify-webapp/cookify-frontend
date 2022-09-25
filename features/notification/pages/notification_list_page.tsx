354a7294043ebc21f2cb6bdd2bd644f08fcf77ba
Pathinya <gift25880@gmail.com>
2022-09-25 14:20:39 +0700

    🎁 update: create route to notification list page

--- features/notification/pages/notification_list_page.tsx
+++ features/notification/pages/notification_list_page.tsx
@@ -1,19 +1,57 @@
-import React,{useEffect} from 'react'
-import {Observer} from 'mobx-react-lite'
+import React, { useContext, useEffect } from "react";
+import { Observer } from "mobx-react-lite";
+import { HomeLayout } from "@core/components/layouts/home_layout";
+import { SearchBox } from "@core/components/input/search_box";
+import { HomeLayoutContext } from "core/context/home_layout_context";
+import { useRouter } from "next/router";
 
-export const NotificationListPage = () =>{
-//---------------------
-// CONTEXT
-//---------------------
+export const NotificationListPage = () => {
+  //---------------------
+  // CONTEXT
+  //---------------------
+  const homeLayoutContext = useContext(HomeLayoutContext)
 
+  //---------------------
+  // ROUTER
+  //---------------------
+  const router = useRouter()
 
-//---------------------
-// EFFECT
-//---------------------
-useEffect(()=>{},[])
+  //---------------------
+  // EFFECT
+  //---------------------
+  useEffect(() => {}, []);
 
-//---------------------
-// RENDER
-//---------------------
-return (<Observer>{()=>(<div>A<div></div></div>)}</Observer>)
-}
+  //---------------------
+  // RENDER
+  //---------------------
+  return (
+    <Observer>
+      {() => (
+        <HomeLayout>
+          <div className="mx-auto xl:max-w-6xl">
+            <div className="px-5 w-full block xl:hidden mt-2">
+              <SearchBox
+                onChange={(value) => {
+                  homeLayoutContext.setValue("searchWord", value);
+                }}
+                height="h-16"
+                placeholder="ค้นหาสูตรอาหารได้ที่นี่"
+                value={homeLayoutContext.searchWord}
+                isButton
+                buttonOnClick={() => {
+                  router.push("/recipes");
+                }}
+              />
+            </div>
+            <div className="px-5 2xl:px-0 mx-auto xl:max-w-6xl">
+              <div className="flex justify-between items-end">
+                <h1 className="pt-8 lg:pt-2 headlineL w-auto">การแจ้งเตือน</h1>
+                <p className="bodyM underline cursor-pointer w-auto" onClick={() => null}>เปลี่ยนเป็นอ่านแล้วทั้งหมด</p>
+              </div>
+            </div>
+          </div>
+        </HomeLayout>
+      )}
+    </Observer>
+  );
+};
