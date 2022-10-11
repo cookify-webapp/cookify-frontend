748bc2da905d9f101dae140e923c4ac8b8cb59f9
Pathinya <gift25880@gmail.com>
2022-10-11 14:54:54 +0700

    🎁 update: init random recipe page

--- features/random/pages/random_recipe_page.tsx
+++ features/random/pages/random_recipe_page.tsx
@@ -1,15 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { Observer } from "mobx-react-lite";
+import { HomeLayoutContext } from "core/context/home_layout_context";
+import { ModalContext } from "core/context/modal_context";
+import { useRouter } from "next/router";
+import { RandomRecipeContext } from "../contexts/random_recipe_context";
+import { HomeLayout } from "@core/components/layouts/home_layout";
+import { SearchBox } from "@core/components/input/search_box";
 
 export const RandomRecipePage = () => {
   //---------------------
   // CONTEXT
   //---------------------
+  const homeLayoutContext = useContext(HomeLayoutContext);
+  const modal = useContext(ModalContext);
+  const context = useContext(RandomRecipeContext);
+
+  //---------------------
+  // ROUTER
+  //---------------------
+  const router = useRouter();
 
   //---------------------
   // EFFECT
   //---------------------
-  useEffect(() => {}, []);
+  useEffect(() => {
+    context.setValue("modal", modal);
+
+    return () => {
+      context.setValue("randomRecipe", null);
+    };
+  }, []);
 
   //---------------------
   // RENDER
@@ -17,9 +37,27 @@ export const RandomRecipePage = () => {
   return (
     <Observer>
       {() => (
-        <div>
-          A<div></div>
-        </div>
+        <HomeLayout>
+          <div className="mx-auto xl:max-w-6xl">
+            <div className="px-5 w-full block xl:hidden mt-2">
+              <SearchBox
+                onChange={(value) => {
+                  homeLayoutContext.setValue("searchWord", value);
+                }}
+                height="h-16"
+                placeholder="ค้นหาสูตรอาหารได้ที่นี่"
+                value={homeLayoutContext.searchWord}
+                isButton
+                buttonOnClick={() => {
+                  router.push("/recipes");
+                }}
+              />
+            </div>
+            <div className="px-5 2xl:px-0 mx-auto xl:max-w-6xl pb-8">
+              <h1 className="headlineL w-auto">สุ่มสูตรอาหาร</h1>
+            </div>
+          </div>
+        </HomeLayout>
       )}
     </Observer>
   );
